c5b68f7f9e94811773df67498339346b8266be30
Amir Mohajerani <amohajerani6@gmail.com>
2023-05-11 17:41:27 -0400

    multi-message

--- app/app.py
+++ app/app.py
@@ -1,30 +1,23 @@
 """
 Python Flask WebApp Auth0 integration example
 """
+from authlib.integrations.flask_client import OAuth
 
 from os import environ as env
 from urllib.parse import quote_plus, urlencode
-from authlib.integrations.flask_client import OAuth
 from dotenv import find_dotenv, load_dotenv
-from flask import Flask, redirect, render_template, session, request, Response, url_for, send_file
+from flask import redirect, render_template, session, request, Response, url_for, send_file
 import orm
-import openai
 import data
-from threading import Thread
-import json
-import time
-import sys
+
 
 ENV_FILE = find_dotenv()
 if ENV_FILE:
     load_dotenv(ENV_FILE)
 
-app = Flask(__name__)
-app.secret_key = env.get("APP_SECRET_KEY")
-openai.api_key = env.get("OPENAI_KEY")
+app = data.init_app()
 
 oauth = OAuth(app)
-
 oauth.register(
     "auth0",
     client_id=env.get("AUTH0_CLIENT_ID"),
@@ -94,41 +87,15 @@ def chat():
     return render_template('chat.html')
 
 
+@app.route('/get_response_test', methods=['POST'])
+def get_response_test():
+    return data.get_response(request.json, 'blank', False)
+
+
 @app.route('/get_response', methods=['POST'])
 @require_auth
 def get_response():
-    # get the payload
-    start = time.time()
-    req_data = request.get_json()
-    user_text = req_data['msg']
-    chat_history = req_data['history']
-    print('step 1: ', time.time()-start, file=sys.stderr)
-
-    # store the user input to db
-    thread_input_txt = Thread(target=orm.insert_chat, args=(
-        session['user']['user_id'], user_text, 'user'))
-    thread_input_txt.start()
-    print('step 2: ', time.time()-start, file=sys.stderr)
-    # get response from the bot
-    messages = [{'role': 'system',
-                 "content": "You help me write a better diary journal by providing brief and thoughtful prompts. Be brief"}]
-    messages.extend(chat_history)
-    messages.append({'role': 'user', 'content': user_text})
-    print('step 3: ', time.time()-start, file=sys.stderr)
-    res = openai.ChatCompletion.create(
-        model="gpt-3.5-turbo",
-        messages=messages,
-        max_tokens=200,
-        temperature=0,
-    )
-    print('step 4: ', time.time()-start, file=sys.stderr)
-
-    # store the bot's response to the db
-    thread_output_txt = Thread(target=orm.insert_chat, args=(
-        session['user']['user_id'], res['choices'][0]['message']['content'], 'bot'))
-    thread_output_txt.start()
-    print('step 5: ', time.time()-start, file=sys.stderr)
-    return res['choices'][0]['message']['content']
+    return data.get_response(request.json, session['user']['user_id'])
 
 
 @ app.route("/past_entries/<date>")
--- app/data.py
+++ app/data.py
@@ -1,3 +1,62 @@
+from flask import request, Flask
+from threading import Thread
+import openai
+import orm
+from dotenv import find_dotenv, load_dotenv
+from os import environ as env
+
+ENV_FILE = find_dotenv()
+if ENV_FILE:
+    load_dotenv(ENV_FILE)
+
+
+def init_app():
+    app = Flask(__name__)
+    app.secret_key = env.get("APP_SECRET_KEY")
+
+    return app
+
+
+openai.api_key = env.get("OPENAI_KEY")
+
+
 def get_user_id(id):
     id = id.replace('auth0|', '')
     return id
+
+
+def get_response(req_data, user_id, store=True):
+    '''
+    Send the user's input to GPT and return the response. 
+    If store is set to True, store this exchange to the db.
+    '''
+    # get the payload
+    user_text = req_data['msg']
+    chat_history = req_data['history']
+
+    # get response from the bot
+    messages = [{'role': 'system',
+                 "content": "You help me write a better diary journal by providing brief and thoughtful prompts. Be brief"}]
+    messages.extend(chat_history)
+    messages.append({'role': 'user', 'content': user_text})
+    if store:
+        thread_input_txt = Thread(target=orm.insert_chat, args=(
+            user_id, user_text, 'user'))
+        thread_input_txt.start()
+    try:
+        res = openai.ChatCompletion.create(
+            model="gpt-3.5-turbo",
+            messages=messages,
+            max_tokens=200,
+            temperature=0,
+        )
+    except Exception as e:
+        print(e)
+        return "Gagali is sorry. She cannot be reached now, you message is safe with her"
+
+    # store the user input and bot's response to db
+    if store:
+        thread_output_txt = Thread(target=orm.insert_chat, args=(
+            user_id, res['choices'][0]['message']['content'], 'bot'))
+        thread_output_txt.start()
+    return res['choices'][0]['message']['content']
