7ef3269845b999888b33d325618130057fb4d8ae
Amir Mohajerani <amohajerani6@gmail.com>
2023-05-15 12:35:01 -0400

    none

--- run.sh
+++ run.sh
@@ -5,7 +5,7 @@ sudo git pull
 sudo docker stop $(sudo docker ps -a -q)
 sudo docker rm $(sudo docker ps -a -q)
 sudo docker network create mynetwork
-sudo docker build -t app ./app
+sudo docker build --memory 4g -t app ./app
 sudo docker build -t nginx ./nginx
 
 sudo docker run --name app -d -p 8000:8000 -p 27017:27017 -p 27016:27016 -p 27015:27015 --network mynetwork app
