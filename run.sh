de771ea809a09b7ae297a295c7eb9755c66c02f7
Amir Mohajerani <amohajerani6@gmail.com>
2023-05-21 10:30:36 -0400

    none

--- run.sh
+++ run.sh
@@ -4,6 +4,7 @@
 sudo git pull
 sudo docker system prune -a
 sudo docker stop $(sudo docker ps -a -q)
+sudo docker rm $(sudo docker ps -a -q)
 sudo docker rmi -f $(sudo docker images -aq)
 sudo docker network create mynetwork
 sudo docker build --memory 4g -t app ./app
