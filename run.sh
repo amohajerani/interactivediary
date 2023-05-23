90d5834855f82fd59a4350eb0f4147ad53ee389b
Amir Mohajerani <amohajerani6@gmail.com>
2023-05-23 15:50:22 -0400

    none

--- run.sh
+++ run.sh
@@ -10,7 +10,7 @@ sudo docker network create mynetwork
 sudo docker build --memory 4g -t app ./app
 sudo docker build -t nginx ./nginx
 
-sudo docker run --name app -d -p 8000:8000 -p 27017:27017 -p 27016:27016 -p 27015:27015 --network mynetwork app
+sudo docker run --name app -d -p 8000:8000 -p 27017:27017 -p 27016:27016 -p 27015:27015 --network mynetwork -e PYTHONUNBUFFERED=1 app
 sudo docker run --name nginx --restart always -d -p 80:80 -p 443:443 --network mynetwork nginx
 
 sudo docker ps
